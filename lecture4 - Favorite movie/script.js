c02f8b69ce8ea7a984d8ea01168e5a04251502ba
marchingbeagle <erik.schneider.pacheco@gmail.com>
2023-02-02 14:32:11 -0300

    Function addMovie broken into 3 functions

--- lecture4 - Favorite movie/script.js
+++ lecture4 - Favorite movie/script.js
@@ -1,5 +1,14 @@
 const movieStorage = [{}];
 
+function clearInput() {
+  document.getElementById("movie").value = "";
+}
+
+function addToStorage() {
+  let movieInput = document.getElementById("movie").value;
+  movieStorage.push("{title:" + movieInput + ",},");
+}
+
 function addMovie() {
   let movieInput = document.getElementById("movie").value;
   let movieList = document.getElementById("movieList");
@@ -7,8 +16,8 @@ function addMovie() {
   if (movieInput.startsWith("https")) {
     movieList.innerHTML +=
       "<img class = 'moviesDisplayed' src=" + movieInput + ">";
-    document.getElementById("movie").value = "";
-    movieStorage.push("{title:" + movieInput + ",},");
+    addToStorage();
+    clearInput();
   } else {
     alert("Endereço invalido, por favor tente novamente");
   }
